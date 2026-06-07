228926c938937599a5ba8a7d5d97e237b1f7e47e
Pritam7620 <chambharepritam98@gmail.com>
2026-06-07 11:13:06 +0530

    Added Spring Boot backend

---
+++ backend/smart-attendance-system-backend/src/main/java/com/SmartAttendanceSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAttendanceSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAttendanceSystemBackendApplication.class, args);
+	}
+
+}
